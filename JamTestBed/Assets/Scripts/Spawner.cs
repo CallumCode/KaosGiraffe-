89fdcc347d72399e4c612995c8ba1fcbf143d963
darkkaos505 <callum.waddell@btinternet.com>
2014-11-03 20:13:08 +0000

    Fixed bugs in swam
    They also spawn in groups

--- JamTestBed/Assets/Scripts/Spawner.cs
+++ JamTestBed/Assets/Scripts/Spawner.cs
@@ -37,31 +37,66 @@ public class Spawner : MonoBehaviour {
 		{
 
 
-			Vector3 pos = PointOnCircle(radius, Random.Range(0,360), TowerObject.transform.position);
 
-			GameObject type = GetEnemyType();
-			pos = new Vector3(pos.x, type.transform.localScale.y , pos.z);
 
-			GameObject enemy = Instantiate(type, pos, Quaternion.identity) as GameObject;
-			enemy.GetComponent<Enemy>().SetUp(TowerObject);
-			enemy.transform.parent = transform;
+				GameObject type = null;
+		float rand = Random.value;
+
+			if (rand > 0.5)
+			{
+				SpawnEnenmy();
+			}
+			else if (rand > 0.2)
+			{
+				SpawnSwarm();
+			}
+
+			else 
+			{
+				SpawnTreeMan();
+			}
+ 		
 		}
 	}
 
-
-
-	GameObject GetEnemyType()
+	void SpawnEnenmy()
 	{
-		GameObject type = null;
-		float rand = Random.value;
+		Vector3 pos = PointOnCircle(radius, Random.Range(0, 360), TowerObject.transform.position);
+		pos = new Vector3(pos.x,EnemyPrefab.transform.localScale.y, pos.z);
 
-		//if (rand > 0.2) type = EnemyPrefab;
-		//else
-			type = SwarmPrefab;
-
-		return type;
+		GameObject enemy = Instantiate(EnemyPrefab, pos, Quaternion.identity) as GameObject;
+		enemy.GetComponent<Enemy>().SetUp(TowerObject);
+		enemy.transform.parent = transform;
 	}
 
+	void SpawnTreeMan()
+	{
+		Vector3 pos = PointOnCircle(radius, Random.Range(0, 360), TowerObject.transform.position);
+		pos = new Vector3(pos.x, TreeManPrefab.transform.localScale.y, pos.z);
+
+		GameObject enemy = Instantiate(TreeManPrefab, pos, Quaternion.identity) as GameObject;
+		enemy.GetComponent<Enemy>().SetUp(TowerObject);
+		enemy.transform.parent = transform;
+	}
+
+
+	void  SpawnSwarm()
+	{ 
+			Vector3 pos = PointOnCircle(radius, Random.Range(0,360), TowerObject.transform.position);
+
+			pos = new Vector3(pos.x, SwarmPrefab.transform.localScale.y , pos.z);
+
+		const int num = 5;
+		for (int i = 0; i < num; i ++ )
+		{
+			GameObject enemy = Instantiate(SwarmPrefab, pos  + transform.right*i , Quaternion.identity) as GameObject;
+			enemy.GetComponent<Enemy>().SetUp(TowerObject);
+			enemy.transform.parent = transform;
+
+		}
+		
+	}
+	 
 	Vector3 PointOnCircle(float radius, float angleInDegrees, Vector3 origin)
 	{
  		float x = (float)(radius * Mathf.Cos(angleInDegrees * Mathf.PI / 180F)) + origin.x;
