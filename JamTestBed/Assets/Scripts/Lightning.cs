54e1d88a7a5ba3ff5f042e7bc03f6c3e662485a1
darkkaos505 <callum.waddell@btinternet.com>
2014-11-09 18:31:25 +0000

    Lightning attack part 1
    Added in some prototype cloud building.
    Use '3' to change to lightning attack mode to test spawn the clouds

---
+++ JamTestBed/Assets/Scripts/Lightning.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lightning : MonoBehaviour
+{
+
+	public GameObject CloudPrefab = null;
+
+	enum LightningStageType { growing, striking, shrinking, dead };
+	LightningStageType lightningStage = LightningStageType.growing;
+
+
+
+	public int maxFirstSubClouds = 10;
+	public int minFirstSubClouds = 5;
+
+	public int maxSubCloudDepth = 10;
+	public int minSubCloudDepth = 5;
+
+	public float firstRadius = 10;
+	public float minSubCloudShrink = 0.25f;
+	public float maxSubCloudShrink = 0.75f;
+	
+	public float flatenScaler = 0.75f;
+	 
+
+	// Use this for initialization
+	void Start()
+	{
+		CreateClouds();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		switch (lightningStage)
+		{
+			case LightningStageType.growing:
+				{
+					GrowingUpdate();
+				}
+				break;
+			case LightningStageType.striking:
+				{
+
+				}
+				break;
+			case LightningStageType.shrinking:
+				{
+
+				}
+				break;
+			case LightningStageType.dead:
+				{
+
+				}
+				break;
+
+		}
+
+	}
+
+	void GrowingUpdate()
+	{
+
+	}
+
+
+	void CreateClouds()
+	{
+
+		if (CloudPrefab != null) 
+		{
+			float numParentCloud = Random.Range(minFirstSubClouds ,maxFirstSubClouds);
+			for (float parentCloud = 0; parentCloud < numParentCloud; parentCloud++)
+			{
+				GameObject cloud = Instantiate(CloudPrefab, transform.position, transform.rotation) as GameObject;
+				cloud.transform.parent = transform;
+				cloud.transform.localScale *= firstRadius;
+
+				AddSubClouds(cloud.transform.position, firstRadius, Random.Range(minSubCloudDepth,maxSubCloudDepth));
+
+			}
+
+		}
+ 
+	}
+
+
+
+
+	void AddSubClouds(Vector3 parentPos, float parentRadius, float deapth)
+	{
+		if (deapth > 0)
+		{
+			deapth--;
+
+			float newRadius = Random.Range(parentRadius * minSubCloudShrink, parentRadius * maxSubCloudShrink);
+
+			Vector3 pos = ( Random.onUnitSphere* newRadius);
+
+			pos = new Vector3(pos.x, pos.y * flatenScaler, pos.z) + parentPos;
+
+
+			GameObject cloud = Instantiate(CloudPrefab, pos, transform.rotation) as GameObject;
+
+			cloud.transform.parent = transform;
+			cloud.transform.localScale *= Mathf.Clamp(newRadius, 0.01f , firstRadius);
+
+			AddSubClouds(pos, newRadius, deapth);
+		}
+	}
+
+	public void FinishGrowing()
+	{
+		lightningStage = LightningStageType.striking;
+	}
+}
--- JamTestBed/Assets/Scripts/Player.cs
+++ JamTestBed/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour 
 {
 
-	public enum AttackStateType { IceWall, FireBall }
+	public enum AttackStateType { IceWall, FireBall, Lightning }
 	public AttackStateType attackState = AttackStateType.IceWall;
 	//
 	public GameObject IceWallPrefab;
@@ -33,7 +33,17 @@ public class Player : MonoBehaviour
 	public float fireBallMasPerTick = 1;
 	private float fireBallMass = 0;
 
+	//
 
+	public GameObject LightningPrefab;
+	private GameObject currentLightning = null;
+
+	private Vector3 lightningStart;
+	private Vector3 lightningEnd;
+
+	private bool growingLightning = false;
+
+	public float lightningHeight = 15;
 
 	// Use this for initialization
 	void Start () 
@@ -61,6 +71,11 @@ public class Player : MonoBehaviour
 			attackState = AttackStateType.IceWall;
 		}
 
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			attackState = AttackStateType.Lightning;
+		}
+
 		///
 		switch (attackState)
 		{
@@ -75,9 +90,51 @@ public class Player : MonoBehaviour
 					FireBallUpdate();
 				}
 				break;
+
+			case AttackStateType.Lightning:
+				{
+					LightningUpdate();
+				}
+				break;
 		}		
 
 	}
+
+
+
+	void LightningUpdate()
+	{
+
+		if (growingLightning  == false && Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit))
+			{
+				lightningStart = hit.point;
+				growingLightning = true;
+				SpawnLightning();
+ 			}
+		}
+
+		if (growingLightning == true && Input.GetMouseButtonUp(0))
+		{
+			Lightning lightning = currentLightning.GetComponent<Lightning>();
+
+			lightning.FinishGrowing();
+
+			currentLightning = null;
+			growingLightning = false;
+		}
+
+	}
+
+	void SpawnLightning()
+	{
+		lightningStart = new Vector3(lightningStart.x, lightningHeight, lightningStart.z);
+		currentLightning = Instantiate(LightningPrefab , lightningStart, Quaternion.identity) as GameObject;
+ 	}
 
 
 	void IceWallUpdate()
